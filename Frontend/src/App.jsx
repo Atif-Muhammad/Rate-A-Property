5b66559ba41da33a80a05077a4dc9c4581f8b46d
atifIgnipulse <atif.ignipulse@gmail.com>
2025-03-11 02:59:46 -0700

    c

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,25 +4,36 @@ import { Home } from "./pages/Home";
 import { NotFound } from "./pages/notfound/NotFound";
 import { Sign_In } from "./pages/authentication/Sign_In";
 import { Sign_Up } from "./pages/authentication/Sign_Up";
-import { NewPost } from "./components/NewPost";
+// import { NewPost } from "./components/NewPost";
 import CommentSection from "./components/Card/CommentSection";
+import { Header } from "./pages/header/Header";
+import { LayoutForLanding } from "./pages/LayoutForLanding";
+import { Notifications } from "./pages/notification/Notifications";
+import { Messages } from "./pages/messages/Messages";
 // import State from "./context/state";
 
 function App() {
   return (
     <>
-          {/* <State> */}
+      {/* <State> */}
       <BrowserRouter>
+        <Header />
         <Routes>
-            <Route path="/" element={<Home />} />
-          <Route path="*" element={<NotFound />} />
+          {/* using outlet in layout */}
+          <Route path="/" element={<LayoutForLanding />}>
+            <Route index element={<Home />} />
+            <Route path="*" element={<NotFound />} />
+            <Route path="/notifications" element={<Notifications />} />
+            <Route path="/messages" element={<Messages />} />
+          </Route>
+          {/* others components */}
+          {/* <Route path="/newPost" element={<NewPost />} /> */}
+          <Route path="/commentsection/:postId" element={<CommentSection />} />
           <Route path="/signin" element={<Sign_In />} />
           <Route path="/signup" element={<Sign_Up />} />
-          <Route path="/newPost" element={<NewPost />} />
-          <Route path="/commentsection/:postId" element={<CommentSection />} />
         </Routes>
       </BrowserRouter>
-          {/* </State> */}
+      {/* </State> */}
     </>
   );
 }
